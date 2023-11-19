93b6202463ac7432b50debaebe4f39b50fbfec32
satyam.su <satyam.su@media.net>
2023-11-20 00:50:47 +0530

    update cb url

--- config/passport.js
+++ config/passport.js
@@ -1,13 +1,15 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../models/user");
 
+const callbackURL = process.env.CALLBACK_URL || "http://localhost:5000/auth/google/callback";
+
 module.exports = (passport) => {
     passport.use(
       new GoogleStrategy(
         {
           clientID: process.env.GOOGLE_CLIENT_ID,
           clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-          callbackURL: "/auth/google/callback",
+          callbackURL: callbackURL,
         },
         async (accessToken, refreshToken, profile, done) => {
           // check if user already exists in db
