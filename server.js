f2eb5219de775d18c2a322d5361b1d46c998d8a1
satyam.su <satyam.su@media.net>
2023-11-19 23:16:39 +0530

    chng port

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(passport.session());
 require("./routes/authRoutes")(app, passport);
 require("./routes/apiRoutes")(app);
 
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
